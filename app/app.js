22c9189e96a02bf882a12e1696afa5c68d0e9f7d
tomi <bkc.tomi1979@gmail.com>
2020-11-15 15:44:09 +0900

    app.jsからDBへの接続

--- app/app.js
+++ app/app.js
@@ -1,27 +1,26 @@
-// import
 // server
 const express = require("express");
 const app = express();
 // db
-// const mysql = require('mysql2');
-// const db_setting = {
-//  host: process.env.DB_HOST || 'localhost',
-//  port: process.env.DB_PORT || 3306,
-//  user: process.env.DB_USER || 'root',
-//  password: process.env.DB_PASSWORD ||'manager',
-//  database: process.env.DB_DATABASE ||'management'
-// };
-// const db = mysql.createConnection(db_setting);
+const mysql = require('mysql2');
+const db_setting = {
+ host: process.env.DB_HOST || 'mysql', // dockerコンテナの場合はホストはサービス名になる。
+ port: process.env.DB_PORT || 3306,
+ user: process.env.DB_USER || 'root',
+ password: process.env.DB_PASSWORD ||'manager',
+ database: process.env.DB_DATABASE ||'management'
+};
+const db = mysql.createConnection(db_setting);
 
 const apiVer = "v1";
 
-// db.connect();
-// db.query('SELECT * from users LIMIT 10;', (err, rows, fields) => {
-//     if (err) throw err;
+db.connect();
+db.query('SELECT * from users LIMIT 10;', (err, rows, fields) => {
+    if (err) throw err;
   
-//     console.log('The solution is: ', rows);
-// });
-// db.end();
+    console.log('The solution is: ', rows);
+});
+db.end();
 
 
 
