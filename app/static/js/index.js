07cb7c4f201d9c23d85c9b6ca0c1e8499d864426
tomi <bkc.tomi1979@gmail.com>
2020-11-18 10:29:03 +0900

    ユーザー検索と登録の表示を作成

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -3,18 +3,25 @@ const indexModule = (() => {
 
     switch (path) {
         case "/":
-            document.getElementById("next-btn")
-            .addEventListener("click", () => {
+            document.getElementById("next-btn").addEventListener("click", () => {
                 return usersModule.nextPage();
             });
-            document.getElementById("prev-btn")
-            .addEventListener("click", () => {
+            document.getElementById("prev-btn").addEventListener("click", () => {
                 return usersModule.prevPage();
             });
-            document.getElementById("show-length")
-            .addEventListener("change", () => {
+            document.getElementById("show-length").addEventListener("change", () => {
                 return usersModule.changeShowLength();
-            })
+            });
+            // 登録ウィンドウの表示
+            document.getElementById("user-submit").addEventListener("click", () => {
+                const page = document.getElementById("submit-bg");
+                page.style.display = "flex";
+            });
+            // 登録ウィンドウの非表示
+            document.getElementById("close-submit").addEventListener("click", () => {
+                const page = document.getElementById("submit-bg");
+                page.style.display = "none";
+            });
             return usersModule.fetchUsers();
         default:
             break;
